8f84b7b0fe3d553b4f9fbe57ac030817718c279a
AlbertAsmervik <Albertasmervik@gmail.com>
2023-03-06 12:52:41 +0100

    Rectangle
    Jeg la til rectangle i mixen

--- ShapeGen4.java
+++ ShapeGen4.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class ShapeGen4 {
+public class ShapeGen4 { 
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int inputSize;
+    
 
     System.out.println("Would you like to create a shape (Y/N)? ");
     String answerInput = sc.nextLine();
@@ -19,7 +20,7 @@ public class ShapeGen4 {
     while (answer == 'Y')
     // hvis svaret er Y, Yes, yes, osv starter while loopen under
     {
-    System.out.println("What shape would you like to print - (T)riangle , (S)quare: ");
+    System.out.println("What shape would you like to print - (T)riangle , (S)quare, (R)ectangle: ");
     String inputString = sc.nextLine();
     String uppercaseInput = inputString.toUpperCase();
     char inputShape = uppercaseInput.charAt(0);
@@ -42,9 +43,15 @@ public class ShapeGen4 {
          Square(inputSize);
          break;
     // Hvis input shape er S,s osv spør brukeren om størrelsen til Firkanten og sender denne verdien til Square metoden på bunnen
-
+        case 'R':
+        System.out.print("Enter the length of the rectangle: ");
+                int reclength = sc.nextInt();
+                System.out.print("Enter the height of the rectangle: ");
+                int recHeight = sc.nextInt();
+                Rectangle(reclength, recHeight);
+        break;
          default: 
-         System.out.println("Invalid input please enter - (T)riangle or (S)quare : ");
+         System.out.println("Invalid input please enter - (T)riangle or (S)quare or (R)ectangle : ");
          break;
     // Hvis brukeren setter inn en ugyldig verdi printes teksten over
      }
@@ -78,8 +85,9 @@ public class ShapeGen4 {
 
     }
 
- public static void Square(int size)
-    {
+ public static void Square(int size) 
+ {
+    
         if ((size < 2)) 
         {
           System.out.println("Invalid number input number is to small");
@@ -89,12 +97,22 @@ public class ShapeGen4 {
         {
          for (int i = 0; i < size; i++) 
          {
-            for (int j = 0; j < size; j++) {
+            for (int j = 0; j < size; j++) 
+            {
                 System.out.print("* ");
-            }
-             System.out.println();
+            } 
+             System.out.println(); 
          }
-          // Ellers printes firekanten med denne nested loopen
+        }
+        }
+
+          public static void Rectangle(int reclength, int recheight) {
+          for (int i = 0; i < recheight; i++) {
+              for (int j = 0; j < reclength; j++) {
+                  System.out.print("* ");
+              }
+              System.out.println(); 
+       
         }
     }
 
